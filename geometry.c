c9af9d4ec978cd5f6aef5d0baf01d0b13214acb7
franciol <fran.aveiro@hotmail.com>
2019-02-25 20:56:33 -0300

    trabalhando no ex 1

--- geometry.c
+++ geometry.c
@@ -1,21 +1,51 @@
 #include <math.h>
+#include <stdio.h>
 
 #include "geometry.h"
 
 int verify(point p, point a, point b) {
 
   //Inicializando Variáveis
-  double m = (a.x - b.x) / (a.y - b.y);        // Coeficiente angular da reta ab
-  double pontoXReta = ((a.y - p.y) / m) - a.x; //Achando o valor de X que cruza com a reta na altura p.y
-  if (p.y < a.y && p.y > b.y || p.y > a.y && p.y < b.y) { //Verifica se p se encontra dentro do intervalo da reta no eixo y
+  double ex, ey, m1, m2, m2y, m2x;
+  m2y = (a.y-b.y);
+  m2x = (a.x-b.x);
+  m2 = m2y/m2x;
+               //Achando o valor de X que cruza com a reta na altura p.y
+  if ((p.y < (a.y+0.000001) && p.y > (b.y-0.000001)) || (p.y > (a.y-0.000001) && p.y < (b.y+0.000001))) { //Verifica se p se encontra dentro do intervalo da reta no eixo y
+    
+  
 
-    if (abs(pontoXReta - p.x) < 0.000001) {     // verifica se p está emcima da reta
-      return 2;
 
-    } else if (p.x < pontoXReta) { // verifica se a reta está a esquerda de p
-      return 1;
+
+    if ((p.y-a.y)<0.000001 || (a.y-p.y)< 0.000001 || (p.y-b.y)< 0.000001 || (b.y-p.y)<0.000001) {
+      m1 = -0.1;
     }
+    else {
+      m1 = 0;
+    }
+
+    ex = ( ((p.y-(p.x*m1)) - (a.y-(a.x*m2)) )/(m2-m1)   );
+    ey = p.y-(m1*(p.x-ex));
+
+    if( (ey<a.y+0.000001 && ey>b.y-0.000001) || (ey>a.y-0.000001 && ey<b.y+0.000001) ){
+      if(ex>p.x){
+        if(ex-p.x<0.000001){
+          return 2;
+        }
+        else{
+          return 1;
+        }
+      }
+      else if(ex<p.x){
+        if(p.x-ex <0.000001){
+          return 2;
+        }
+      }
+    
+    }
+
   }
+  
   return 0; // caso a reta não se encontre a direita ou emcima de p, retorna zero
 }
 
