ef0aa8d6c6fe1fa294888b9382365706453d85d0
franciol <fran.aveiro@hotmail.com>
2019-02-27 13:14:54 +0000

    Atualizando nomes de variáveis para facilitar entedimento

--- geometry.c
+++ geometry.c
@@ -6,7 +6,7 @@
 int verify(point p, point a, point b) {
 
   //Inicializando Variáveis
-  double ex, ey, m1, m2, m2y, m2x;
+  double encontroX, encontroY, m1, m2, m2y, m2x;   //m sendo coeficiente angular (O segundo teve que ser quebrado em 3 partes para cuidar do caso de m2x == 0 
   double difMinima = 0.000001;
   if ((p.y < (a.y+difMinima) && p.y > (b.y-difMinima)) || (p.y > (a.y-difMinima) && p.y < (b.y+difMinima))) {     //Verifica se p se encontra dentro do intervalo da reta no eixo y
     m2y = (a.y-b.y);                                                                                              //Achando o coeficiente angular do segmento AB   
@@ -23,23 +23,23 @@ int verify(point p, point a, point b) {
 
 
     if(m2x<difMinima && m2x >(-difMinima)){                                                                 //Verifica um caso do coeficiente angular infinito, para tratar separadamente
-       ex = a.x;
+       encontroX = a.x;
     }
     else{
-      ex = ( ((p.y-(p.x*m1)) - (a.y-(a.x*m2)) )/(m2-m1)   );
+      encontroX = ( ((p.y-(p.x*m1)) - (a.y-(a.x*m2)) )/(m2-m1)   );
     }
-    ey = p.y-(m1*(p.x-ex));
-    if( ((ey<a.y+difMinima && ey>b.y-difMinima) || (ey>a.y-difMinima && ey<b.y+difMinima)) && ((a.x-difMinima<ex&&b.x+difMinima>ex)||(a.x+difMinima>ex&&b.x-difMinima<ex)) ){                   // Verifica se o ponto de encontro das 2 retas esta dentro dos eixos xy do segmento de reta
-      if(ex>p.x-difMinima){
-        if(ex<difMinima+p.x&&((a.x-difMinima<ex&&b.x+difMinima>ex)||(a.x+difMinima>ex&&b.x-difMinima<ex))){
+    encontroY = p.y-(m1*(p.x-encontroX));
+    if( ((encontroY<a.y+difMinima && encontroY>b.y-difMinima) || (encontroY>a.y-difMinima && encontroY<b.y+difMinima)) && ((a.x-difMinima<encontroX&&b.x+difMinima>encontroX)||(a.x+difMinima>encontroX&&b.x-difMinima<encontroX)) ){                   // Verifica se o ponto de encontro das 2 retas esta dentro dos eixos xy do segmento de reta
+      if(encontroX>p.x-difMinima){
+        if(encontroX<difMinima+p.x&&((a.x-difMinima<encontroX&&b.x+difMinima>encontroX)||(a.x+difMinima>encontroX&&b.x-difMinima<encontroX))){
           return 2;
         }
         else{
           return 1;
         }
       }
-      else if(ex<p.x+difMinima){
-        if(p.x <difMinima+ex){
+      else if(encontroX<p.x+difMinima){
+        if(p.x <difMinima+encontroX){
           return 2;
         }
       }
@@ -52,19 +52,19 @@ int verify(point p, point a, point b) {
 
 int inside(point p, point poly[], int n) {
   int sum = 0;   //Inicializando variaveis
-  int ret;
+  int boolLado;
   for(int i = 0;i<n;i++){ //Rodando para todos os lados
     if(i==n-1){           //Verificar se é o lado composto pelo 1° e ultimo ponto
-      ret = verify(p,poly[i],poly[0]);
+      boolLado = verify(p,poly[i],poly[0]);
     }
     else{
-      ret = verify(p, poly[i], poly[i+1]);
+      boolLado = verify(p, poly[i], poly[i+1]);
     }
-    if(ret==2){           //Verifica se está acima da linha
+    if(boolLado==2){           //Verifica se está acima da linha
       return 1;
       break;
     }
-    sum+=ret;             //Soma das saidas de verify
+    sum+=boolLado;             //Soma das saidas de verify
   }
   if(sum%2==0){           //Se a soma for par o ponto p está fora da imagem
     return 0;
