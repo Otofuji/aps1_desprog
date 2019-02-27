c8491d8324a876cabf2e98a955a7b1ed67d09701
franciol <fran.aveiro@hotmail.com>
2019-02-27 12:56:07 +0000

    APS pronta

--- geometry.c
+++ geometry.c
@@ -1,43 +1,45 @@
 #include <math.h>
-#include <stdio.h>
 
 #include "geometry.h"
+
 
 int verify(point p, point a, point b) {
 
   //Inicializando Variáveis
   double ex, ey, m1, m2, m2y, m2x;
-  m2y = (a.y-b.y);
-  m2x = (a.x-b.x);
-  m2 = m2y/m2x;
-               //Achando o valor de X que cruza com a reta na altura p.y
-  if ((p.y < (a.y+0.000001) && p.y > (b.y-0.000001)) || (p.y > (a.y-0.000001) && p.y < (b.y+0.000001))) { //Verifica se p se encontra dentro do intervalo da reta no eixo y
-    
-  
-
-
-
-    if ((p.y-a.y)<0.000001 || (a.y-p.y)< 0.000001 || (p.y-b.y)< 0.000001 || (b.y-p.y)<0.000001) {
-      m1 = -0.1;
+  double difMinima = 0.000001;
+  if ((p.y < (a.y+difMinima) && p.y > (b.y-difMinima)) || (p.y > (a.y-difMinima) && p.y < (b.y+difMinima))) {     //Verifica se p se encontra dentro do intervalo da reta no eixo y
+    m2y = (a.y-b.y);                                                                                              //Achando o coeficiente angular do segmento AB   
+    m2x = (a.x-b.x);
+    m2 = m2y/m2x;             
+ 
+    if ( ( (a.y-p.y)<difMinima && (a.y-p.y)>(-difMinima) ) || ((p.y-b.y) < difMinima && (p.y-b.y) > (-difMinima))) {         //Verifica se precisa aplicar uma perturbação no ray cast     
+      m1 = -0.1;                                                                                                             //Coeficiente angular de ray cast perturbado
     }
     else {
       m1 = 0;
     }
 
-    ex = ( ((p.y-(p.x*m1)) - (a.y-(a.x*m2)) )/(m2-m1)   );
-    ey = p.y-(m1*(p.x-ex));
 
-    if( (ey<a.y+0.000001 && ey>b.y-0.000001) || (ey>a.y-0.000001 && ey<b.y+0.000001) ){
-      if(ex>p.x){
-        if(ex-p.x<0.000001){
+
+    if(m2x<difMinima && m2x >(-difMinima)){                                                                 //Verifica um caso do coeficiente angular infinito, para tratar separadamente
+       ex = a.x;
+    }
+    else{
+      ex = ( ((p.y-(p.x*m1)) - (a.y-(a.x*m2)) )/(m2-m1)   );
+    }
+    ey = p.y-(m1*(p.x-ex));
+    if( ((ey<a.y+difMinima && ey>b.y-difMinima) || (ey>a.y-difMinima && ey<b.y+difMinima)) && ((a.x-difMinima<ex&&b.x+difMinima>ex)||(a.x+difMinima>ex&&b.x-difMinima<ex)) ){                   // Verifica se o ponto de encontro das 2 retas esta dentro dos eixos xy do segmento de reta
+      if(ex>p.x-difMinima){
+        if(ex<difMinima+p.x&&((a.x-difMinima<ex&&b.x+difMinima>ex)||(a.x+difMinima>ex&&b.x-difMinima<ex))){
           return 2;
         }
         else{
           return 1;
         }
       }
-      else if(ex<p.x){
-        if(p.x-ex <0.000001){
+      else if(ex<p.x+difMinima){
+        if(p.x <difMinima+ex){
           return 2;
         }
       }
@@ -45,10 +47,27 @@ int verify(point p, point a, point b) {
     }
 
   }
-  
   return 0; // caso a reta não se encontre a direita ou emcima de p, retorna zero
 }
 
 int inside(point p, point poly[], int n) {
-  return 0;
+  int sum = 0;   //Inicializando variaveis
+  int ret;
+  for(int i = 0;i<n;i++){ //Rodando para todos os lados
+    if(i==n-1){           //Verificar se é o lado composto pelo 1° e ultimo ponto
+      ret = verify(p,poly[i],poly[0]);
+    }
+    else{
+      ret = verify(p, poly[i], poly[i+1]);
+    }
+    if(ret==2){           //Verifica se está acima da linha
+      return 1;
+      break;
+    }
+    sum+=ret;             //Soma das saidas de verify
+  }
+  if(sum%2==0){           //Se a soma for par o ponto p está fora da imagem
+    return 0;
+  }
+  return 1;
 }
